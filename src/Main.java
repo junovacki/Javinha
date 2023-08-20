e3f56e2cb75ba985dd4b6c2540ffe1414fdeb4e2
junovacki <juliianonovacki@gmail.com>
2023-08-20 17:13:03 -0300

    Alterando o Hello World tentando aplicar conceitos de SOLID

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,18 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello world!");
+       escreverFrase("Hello World!");
+       incrementando(5);
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static void escreverFrase(String frase){
+        System.out.println(frase);
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void incrementando(int parada){
+        for (int i = 1; i <= parada; i++) {
+            String frase = "i = " + i;
+            escreverFrase(frase);
         }
     }
 }
