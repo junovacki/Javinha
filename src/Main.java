6485d3bca0e046ae21b6b8f6f9cdfeb60086507b
junovacki <juliianonovacki@gmail.com>
2023-08-20 07:36:11 -0300

    Programa que pede a quantidade de usuario e depois o valor do salario de cada funcionario, para depois informar a media salarial

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello world!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Insira a quantidade de pessoas: ");
+        int qtdPessoas = teclado.nextInt();
+        double valorSalario=0,valorFinal,valorAtual;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        if(qtdPessoas > 0){
+            for (int i =0; i < qtdPessoas; i++){
+                System.out.println("Insira o valor de salario do funcionario: ");
+                valorAtual = teclado.nextDouble();
+                valorSalario = valorSalario + valorAtual;
+            }
+            valorFinal = valorSalario / qtdPessoas;
+            System.out.println("Valor da media salarial: "+ valorFinal);
+
+        }else{
+            System.out.println("Insira um numero maior que zero");
         }
+
     }
 }
